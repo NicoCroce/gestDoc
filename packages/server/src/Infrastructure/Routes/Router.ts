40670fd8b59c8773e6dbddd2b2b95c0b457574c1
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-17 17:44:07 -0300

    feat(server): added routes

--- packages/server/src/Infrastructure/Routes/Router.ts
+++ packages/server/src/Infrastructure/Routes/Router.ts
@@ -2,8 +2,9 @@ import { Express } from 'express';
 import { UserRoutes } from '@server/domains/Users';
 import { router, trpcExpress, createContext } from '../trpc';
 import { AuthRoutes } from '@server/domains/Auth/Infrastructure/Routes';
+import { DocumentsRoutes } from '@server/domains/Documents/Infrastructure/Routes';
 
-const AllRouters = { ...UserRoutes, ...AuthRoutes };
+const AllRouters = { ...UserRoutes, ...AuthRoutes, ...DocumentsRoutes };
 
 const MainRouter = router(AllRouters);
 

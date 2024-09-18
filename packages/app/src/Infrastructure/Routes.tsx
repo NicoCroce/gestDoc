58a665e97fbd0b95d9235ec9c9e30d8846ea2136
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-18 02:05:45 -0300

    feat(app): added route for Documents

--- packages/app/src/Infrastructure/Routes.tsx
+++ packages/app/src/Infrastructure/Routes.tsx
@@ -1,5 +1,6 @@
 import { AuthRouter } from '@app/Domains/Auth';
+import { DocumentsRouter } from '@app/Domains/Documents/';
 import { MainRouter } from '@app/Domains/Main';
 import { UsersRouter } from '@app/Domains/Users';
 
-export const AllRoutes = [MainRouter, AuthRouter, UsersRouter];
+export const AllRoutes = [MainRouter, AuthRouter, UsersRouter, DocumentsRouter];

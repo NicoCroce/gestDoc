9b4866199ad8f3f8b690a766da6ef91836836056
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-25 10:05:58 -0300

    feat(app): improve document cache

--- packages/app/src/Domains/Documents/Hooks/useGetDocument.ts
+++ packages/app/src/Domains/Documents/Hooks/useGetDocument.ts
@@ -1,32 +1,41 @@
 import { useEffect, useMemo, useState } from 'react';
-import { TDocument } from '../Document.entity';
+import { TDocument, TDocumentSearch, VALIDATED } from '../Document.entity';
 import { documentsService } from '../Documents.service';
 import { useCacheDocuments } from './useCacheDocuments';
+import { useURLParams } from '@app/Aplication';
 
 export const useGetDocument = (id: string | undefined = '') => {
   const [currentDocument, setCurrentDocument] = useState<TDocument | null>(
     null,
   );
-  const queryDocumentDetail = documentsService.get.useQuery(id, {
-    enabled: id !== undefined,
+  const queryDocument = documentsService.get.useQuery(id, {
+    enabled: false,
   });
+  const { searchParams } = useURLParams<TDocumentSearch>();
 
   const cacheDocumentsList = useCacheDocuments();
-  const { isFetched, isFetching, refetch } = queryDocumentDetail;
+  const { isFetched, isFetching, refetch } = queryDocument;
 
   // Extraemos los datos de la caché si es que existe.
-  const cachedDocuments = useMemo(
-    () =>
-      cacheDocumentsList.getData()?.find((document) => document.id === id) ||
-      null,
-    [cacheDocumentsList, id],
-  );
+  const cachedDocuments = useMemo(() => {
+    const { id, ...filterSearchParams } = searchParams || {};
+
+    return (
+      cacheDocumentsList
+        .getData({
+          ...filterSearchParams,
+          signed: filterSearchParams?.signed === VALIDATED,
+          view: filterSearchParams?.signed === VALIDATED,
+        })
+        ?.find((document) => document.id === id) || null
+    );
+  }, [cacheDocumentsList, searchParams]);
 
   useEffect(() => {
-    // Si el usuario está en caché, lo usamos, de lo contrario, hacemos fetch
+    // Si el documento está en caché, lo usamos, de lo contrario, hacemos fetch
     if (cachedDocuments) {
       setCurrentDocument(cachedDocuments);
-    } else if (!isFetching && !isFetched) {
+    } else if (!isFetching && !isFetched && id) {
       refetch().then((res) => {
         setCurrentDocument(res.data || null);
       });
@@ -35,6 +44,6 @@ export const useGetDocument = (id: string | undefined = '') => {
 
   return {
     currentDocument,
-    ...queryDocumentDetail,
+    ...queryDocument,
   };
 };

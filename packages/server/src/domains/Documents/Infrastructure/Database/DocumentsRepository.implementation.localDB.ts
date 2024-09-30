e700c4de34c1345ccce290438eca18d10389230a
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-30 12:42:04 -0300

    feat(server): update name of local database implementation

---
+++ packages/server/src/domains/Documents/Infrastructure/Database/DocumentsRepository.implementation.localDB.ts
@@ -0,0 +1,52 @@
+import {
+  Document,
+  DocumentRepository,
+  IGetDocumentRepository,
+  IGetDocumentsRepository,
+  ISignDocumentRepository,
+  IViewDocumentRepository,
+} from '../../Domain';
+import { DocumentsSchemeLocal } from './Documents.scheme.local';
+
+export class DocumentsRepositoryImplementationLocal
+  implements DocumentRepository
+{
+  private readonly DB = new DocumentsSchemeLocal();
+
+  async getDocuments({
+    filters,
+    requestContext,
+  }: IGetDocumentsRepository): Promise<Document[]> {
+    console.log('Hacer algo con userID', requestContext);
+    const documents = await this.DB.getDocuments(filters);
+    return documents?.map((document) => Document.create(document));
+  }
+
+  async getDocument({
+    id,
+    requestContext,
+  }: IGetDocumentRepository): Promise<Document | null> {
+    console.log('Hacer algo con userID', requestContext);
+    const document = await this.DB.getDocument(id);
+    if (!document) return null;
+    return Document.create(document);
+  }
+
+  viewDocument({
+    requestContext,
+    id,
+  }: IViewDocumentRepository): Promise<number | null> {
+    console.log('Hacer algo con userID', requestContext);
+    return this.DB.viewDocument(id);
+  }
+
+  signDocument({
+    requestContext,
+    id,
+    validationSign,
+    agreement,
+  }: ISignDocumentRepository): Promise<number | null> {
+    console.log('Hacer algo con userID', requestContext);
+    return this.DB.signDocument(id, agreement, validationSign);
+  }
+}

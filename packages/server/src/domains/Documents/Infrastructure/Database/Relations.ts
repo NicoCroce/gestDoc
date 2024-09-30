5699e0529c1950aed9bf2e0f4932f64daf04ca05
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-30 19:37:20 -0300

    feat(server): fix relations

--- packages/server/src/domains/Documents/Infrastructure/Database/Relations.ts
+++ packages/server/src/domains/Documents/Infrastructure/Database/Relations.ts
@@ -4,5 +4,5 @@ import { Documentos, Sis_tipo_documentos } from './Schemes';
 Sis_tipo_documentos.hasMany(Documentos, { foreignKey: 'tipo' });
 Documentos.belongsTo(Sis_tipo_documentos, { foreignKey: 'tipo' });
 
-Documentos.hasMany(UserScheme, { foreignKey: 'Usuario_id' });
-UserScheme.belongsTo(Documentos, { foreignKey: 'Usuario_id' });
+UserScheme.hasMany(Documentos, { foreignKey: 'Usuario_id' });
+Documentos.belongsTo(UserScheme, { foreignKey: 'Usuario_id' });

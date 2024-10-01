f795eabe6fe22bec30d4dab0c993182201d429e6
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-10-01 02:37:31 -0300

    feat(app): added new viewer component for mobile

---
+++ packages/app/src/Domains/Documents/Components/PDFPreview/PDFPreviewMobile.tsx
@@ -0,0 +1,45 @@
+import { Button, Container, useURLParams } from '@app/Aplication';
+import { SignDocument } from '../SignDocument';
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from '@app/Aplication/Components/ui/drawer';
+import { TDocumentSearch } from '../../Document.entity';
+import { useEffect, useState } from 'react';
+
+interface PDFPreviewMobileProps {
+  file: string;
+}
+
+export const PDFPreviewMobile = ({ file }: PDFPreviewMobileProps) => {
+  const { searchParams } = useURLParams<TDocumentSearch>();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (searchParams?.id) setIsOpen(true);
+  }, [searchParams?.id]);
+
+  return (
+    <Drawer open={isOpen} onOpenChange={() => setIsOpen(false)}>
+      <DrawerContent className="h-[90%]">
+        <DrawerHeader className="justify-start">
+          <DrawerTitle>Puedes firmar el documento</DrawerTitle>
+        </DrawerHeader>
+        <Container className="p-4 h-full">
+          <SignDocument />
+          <iframe src={file} height="100%" width="100%"></iframe>
+        </Container>
+        <DrawerFooter>
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};

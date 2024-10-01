a040eb2902434ef01392c26a1def19132334e94b
Nicolás Croce <ingcrocenicolas@outlook.com>
2024-09-30 23:24:59 -0300

    feat(app): hide PDFViewer if is mobile device

--- packages/app/src/Domains/Documents/Pages/DocumentsList.page.tsx
+++ packages/app/src/Domains/Documents/Pages/DocumentsList.page.tsx
@@ -1,20 +1,24 @@
-import { Container, Page } from '@app/Aplication';
+import { Container, Page, useGlobalStore } from '@app/Aplication';
 import { DocumentsListWrapper, PDFPreview, SignDocument } from '../Components';
 
 export const DocumentsListPage = () => {
+  const { data: isMobile } = useGlobalStore('isMobile');
+
   return (
-    <Page title="Documentos" headerRight={<SignDocument />}>
+    <Page title="Documentos" headerRight={!isMobile && <SignDocument />}>
       <Container row>
-        <div className="min-w-[350px] max-w-[400px]">
+        <div className="min-w-[350px] max-w-[400px] w-full">
           <DocumentsListWrapper />
         </div>
-        <Container
-          justify="center"
-          align="center"
-          className="w-full h-[80vh] p-4 border"
-        >
-          <PDFPreview />
-        </Container>
+        {!isMobile && (
+          <Container
+            justify="center"
+            align="center"
+            className="w-full h-[80vh] p-4 border"
+          >
+            <PDFPreview />
+          </Container>
+        )}
       </Container>
     </Page>
   );

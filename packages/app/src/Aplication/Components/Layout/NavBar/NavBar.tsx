80f76b025c8e8b34e3e15abc8a8aec3eba115167
Nico Croce <ingcrocenicolas@outlook.com>
2024-10-23 14:57:27 -0300

    feat(app): change access from Users to Documents

--- packages/app/src/Aplication/Components/Layout/NavBar/NavBar.tsx
+++ packages/app/src/Aplication/Components/Layout/NavBar/NavBar.tsx
@@ -1,12 +1,12 @@
-import { USERS_ROUTE } from '@app/Domains/Users/Users.routes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faUser,
   faArrowRightFromBracket,
+  faFile,
 } from '@fortawesome/free-solid-svg-icons';
 import { NavLink, NavLinkRenderProps } from 'react-router-dom';
 import { Container } from '../Container';
 import { NavBarHeader } from './NavBarHeader';
+import { DOCUMENTS_ROUTE } from '@app/Domains/Documents';
 
 export const styleLink =
   'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary';
@@ -21,9 +21,9 @@ export const NavBar = ({ className = '' }: { className?: string }) => {
       <NavBarHeader />
       <nav className="flex flex-col h-full justify-between mt-4">
         <Container className="flex flex-col gap-2 md:p-4">
-          <NavLink to={USERS_ROUTE} className={isActiveLink}>
-            <FontAwesomeIcon icon={faUser} />
-            Usuarios
+          <NavLink to={DOCUMENTS_ROUTE} className={isActiveLink}>
+            <FontAwesomeIcon icon={faFile} />
+            Documentos
           </NavLink>
         </Container>
         <Container className="flex flex-col gap-2 md:p-4">

79471fe3e15b952a8bb4a9309298811f75760afe
Nico Croce <ingcrocenicolas@outlook.com>
2024-11-07 16:54:49 -0300

    feat(app): added services to change password

--- packages/server/src/domains/Auth/Application/Auth.service.ts
+++ packages/server/src/domains/Auth/Application/Auth.service.ts
@@ -1,16 +1,20 @@
 import { executeUseCase } from '@server/Application';
-import { Login } from '../Domain';
+import { ChangePasswordPublic, Login } from '../Domain';
 import {
+  IChangePasswordPublic,
   IExecuteResponse,
   Ilogin,
   IRestorePassword,
 } from '../Domain/auth.interfaces';
 import { RestorePassword } from '../Domain/UseCases/RestorePassword.usecase';
+import { TRPCError } from '@trpc/server';
+import { verifyToken } from '@server/utils/JWT';
 
 export class AuthService {
   constructor(
     private readonly _login: Login,
     private readonly _restorePassword: RestorePassword,
+    private readonly _changePasswordPublic: ChangePasswordPublic,
   ) {}
 
   async login({ input, requestContext }: Ilogin): Promise<IExecuteResponse> {
@@ -31,6 +35,37 @@ export class AuthService {
     return executeUseCase({
       useCase: this._restorePassword,
       input,
+      requestContext,
+    });
+  }
+
+  async changePasswordPublic({
+    input,
+    requestContext,
+  }: IChangePasswordPublic): Promise<void> {
+    const { token, newPassword, rePassword } = input;
+
+    if (!token) {
+      throw new TRPCError({
+        message: 'Token not provided',
+        code: 'UNAUTHORIZED',
+      });
+    }
+
+    let dataToken;
+
+    try {
+      dataToken = (await verifyToken(token)) as { mail: string };
+    } catch {
+      throw new TRPCError({
+        message: 'Token error',
+        code: 'UNAUTHORIZED',
+      });
+    }
+
+    return executeUseCase({
+      useCase: this._changePasswordPublic,
+      input: { mail: dataToken.mail, newPassword, rePassword },
       requestContext,
     });
   }

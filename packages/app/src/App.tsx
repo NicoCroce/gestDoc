cc1682daf0a029517df1fd47b2340eacb40fe9a8
Nico Croce <ingcrocenicolas@outlook.com>
2024-11-08 09:21:56 -0300

    feat(app): avoid render ChangePasswordModal component if user is in a Public page

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -5,12 +5,14 @@ import { AllRoutes } from './Infrastructure';
 
 import './App.css';
 import { ChangePasswordModal } from './Domains/Users/Components/ChangePassword/ChangePasswordModal';
+import { usePublicPages } from './Aplication';
 
 export const App = () => {
+  const isPublicPage = usePublicPages();
   return (
     <>
       <Routes>{AllRoutes}</Routes>
-      <ChangePasswordModal />
+      {!isPublicPage && <ChangePasswordModal />}
       <Toaster richColors />
     </>
   );

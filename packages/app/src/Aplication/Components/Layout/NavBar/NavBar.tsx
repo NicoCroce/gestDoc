86b475b79109a1a3ec131d248058ae1aab55b5aa
Nico Croce <ingcrocenicolas@outlook.com>
2024-12-08 11:47:21 -0300

    feat(app): improve route for dashboard

--- packages/app/src/Aplication/Components/Layout/NavBar/NavBar.tsx
+++ packages/app/src/Aplication/Components/Layout/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import { USERS_CHANGE_PASSWORD } from '@app/Domains/Users/Users.routes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faArrowRightFromBracket,
+  faChartLine,
   faFile,
   faUser,
 } from '@fortawesome/free-solid-svg-icons';
@@ -9,7 +10,7 @@ import { NavLink, NavLinkRenderProps } from 'react-router-dom';
 import { Container } from '../Container';
 import { NavBarHeader } from './NavBarHeader';
 import { useDevice } from '@app/Aplication/Hooks';
-import { DOCUMENTS_ROUTE } from '@app/Domains/Documents';
+import { DOCUMENTS_DASHBOARD, DOCUMENTS_ROUTE } from '@app/Domains/Documents';
 
 export const styleLink =
   'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary';
@@ -26,6 +27,10 @@ export const NavBar = ({ className = '' }: { className?: string }) => {
       <NavBarHeader />
       <nav className="flex flex-col h-full justify-between mt-4">
         <Container className="flex flex-col gap-2 md:p-4">
+          <NavLink to={DOCUMENTS_DASHBOARD} className={isActiveLink}>
+            <FontAwesomeIcon icon={faChartLine} />
+            Administrar
+          </NavLink>
           <NavLink to={DOCUMENTS_ROUTE} className={isActiveLink}>
             <FontAwesomeIcon icon={faFile} />
             Documentos

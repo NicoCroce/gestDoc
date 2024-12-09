8a1796d77f0bf9533d6b3fdee3c4e8a79d4d7702
Nico Croce <ingcrocenicolas@outlook.com>
2024-12-09 11:13:27 -0300

    feat(server): added usecase to get statistics

--- packages/server/src/domains/Documents/Domain/UseCases/index.ts
+++ packages/server/src/domains/Documents/Domain/UseCases/index.ts
@@ -1,5 +1,6 @@
 export * from './GetDocument.usecase';
 export * from './GetDocuments.usecase';
 export * from './GetDocumentsByCompany.usecase';
+export * from './GetStatisticsDocuments.usecase';
 export * from './SignDocument.usecase';
 export * from './ViewDocument.usecase';

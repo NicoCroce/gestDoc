a0531d1d22499b578d5c91e581cf5ad899b00f9e
Nico Croce <ingcrocenicolas@outlook.com>
2025-02-07 10:49:51 -0300

    feat(app): implement in main router, adding access for Certificates navigation

--- packages/app/src/Infrastructure/Routes.tsx
+++ packages/app/src/Infrastructure/Routes.tsx
@@ -1,5 +1,11 @@
 import { AuthRouter } from '@app/Domains/Auth';
+import { CertificatesRouter } from '@app/Domains/Certificates';
 import { DocumentsRouter } from '@app/Domains/Documents/';
 import { UsersRouter } from '@app/Domains/Users';
 
-export const AllRoutes = [AuthRouter, UsersRouter, DocumentsRouter];
+export const AllRoutes = [
+  AuthRouter,
+  UsersRouter,
+  DocumentsRouter,
+  CertificatesRouter,
+];

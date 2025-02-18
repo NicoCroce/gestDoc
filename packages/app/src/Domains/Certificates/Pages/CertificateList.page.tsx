d5a5209ae5e80a7621bc242856a799f8f1cd13d6
Nico Croce <ingcrocenicolas@outlook.com>
2025-02-18 09:15:15 -0300

    feat(app): added Page to list grid certificates

--- packages/app/src/Domains/Certificates/Pages/CertificateList.page.tsx
+++ packages/app/src/Domains/Certificates/Pages/CertificateList.page.tsx
@@ -1,8 +1,12 @@
-import { Button, Page, Text } from '@app/Aplication';
+import { Button, Container, Page, Text } from '@app/Aplication';
 import { CertificatesGrid } from '../Components/CertificatesGrid';
 import { faAdd } from '@fortawesome/free-solid-svg-icons';
+import { useGetCertificates } from '../Hooks/useGetCertificates';
+import { v4 as uuidv4 } from 'uuid';
 
 export const CertificateListPage = () => {
+  const { data } = useGetCertificates();
+
   return (
     <Page
       title="Certificados"
@@ -12,10 +16,13 @@ export const CertificateListPage = () => {
         </Button>
       }
     >
-      <Text.Lead>Certificados correspondientes al año 2025</Text.Lead>
-      <CertificatesGrid />
-      <Text.Lead>Certificados correspondientes al año 2024</Text.Lead>
-      <CertificatesGrid />
+      {data &&
+        Object.entries(data).map(([year, certificates]) => (
+          <Container key={uuidv4()} space="large">
+            <Text.Lead>Certificados correspondientes al año {year}</Text.Lead>
+            <CertificatesGrid certificatesList={certificates} />
+          </Container>
+        ))}
     </Page>
   );
 };

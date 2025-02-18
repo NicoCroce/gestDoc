c9c8de8687afdadf28d7b2913877ff29e4d9c6d9
Nico Croce <ingcrocenicolas@outlook.com>
2025-02-18 09:16:18 -0300

    feat(app): added certificates services and menu access

---
+++ packages/app/src/Domains/Certificates/MenuCertificates.tsx
@@ -0,0 +1,11 @@
+import { MenuItem } from '@app/Aplication';
+import { CERTIFICATES_ROUTES } from './Certificates.routes';
+import { faFileContract } from '@fortawesome/free-solid-svg-icons';
+
+export const MenuCertificates = () => (
+  <MenuItem
+    to={CERTIFICATES_ROUTES}
+    icon={faFileContract}
+    text="Mis Certificados"
+  />
+);

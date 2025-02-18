d3d7abe0d51c6cb0378f5bbca1ec69a3202a0483
Nico Croce <ingcrocenicolas@outlook.com>
2025-02-18 09:16:59 -0300

    refactor(app): improve menu structure in Documents and Main Domains

--- packages/app/src/Domains/MenuAccess.tsx
+++ packages/app/src/Domains/MenuAccess.tsx
@@ -1,7 +1,8 @@
 import { Container } from '@app/Aplication';
 import { MenuUsers } from './Users';
-import { MenuMain } from './Main';
 import { MenuAuth } from './Auth';
+import { MenuDashboard, MenuDocuments } from './Documents/MenuDocuments';
+import { MenuCertificates } from './Certificates/MenuCertificates';
 
 export const styleLink =
   'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary';
@@ -9,8 +10,10 @@ export const styleLink =
 export const MenuAccess = () => (
   <>
     <Container id="sections" space="small">
-      <MenuMain />
+      <MenuDashboard />
       <MenuUsers />
+      <MenuDocuments />
+      <MenuCertificates />
     </Container>
     <Container id="footer" space="small">
       <MenuAuth />

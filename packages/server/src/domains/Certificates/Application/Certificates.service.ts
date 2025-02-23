66fa8afd1283d2e4442397d4b8f07846d6821474
Nico Croce <ingcrocenicolas@outlook.com>
2025-02-23 12:25:15 -0300

    feat(server): added service and digest improving the response

--- packages/server/src/domains/Certificates/Application/Certificates.service.ts
+++ packages/server/src/domains/Certificates/Application/Certificates.service.ts
@@ -1,19 +1,52 @@
-import { executeUseCase } from '@server/Application';
 import {
+  AppError,
+  executeUseCase,
+  IRequestContext,
+  normalizeDate,
+} from '@server/Application';
+import {
+  AddCertificate,
+  Certificate,
   GetCertificates,
   GetCertificateTypes,
+  ICertificate,
   IGetCertificates,
   IGetCertificateTypes,
 } from '../Domain';
+import { CertificateDTO, IGetCertificatesDTO } from './DTO/CertificateDTO';
+import { convertToDTO } from './digest';
+
+interface IAddCertificateService extends IRequestContext {
+  input: Omit<ICertificate, 'startDate' | 'endDate' | 'type'> & {
+    startDate: string;
+    endDate: string;
+    type: number;
+  };
+}
 
 export class CertificatesServices {
   constructor(
     private readonly _getCertificates: GetCertificates,
     private readonly _getCertificateTypes: GetCertificateTypes,
+    private readonly _addCertificate: AddCertificate,
   ) {}
 
-  getCertificates({ requestContext }: IGetCertificates) {
-    return executeUseCase({ useCase: this._getCertificates, requestContext });
+  async getCertificates({
+    requestContext,
+  }: IGetCertificates): Promise<IGetCertificatesDTO> {
+    const certificates = await executeUseCase({
+      useCase: this._getCertificates,
+      requestContext,
+    });
+
+    // Convierte la respuesta del caso de uso en un objeto más simple.
+    return Object.entries(certificates).reduce((response, [year, certs]) => {
+      response[Number(year)] = certs.map((certificate: Certificate) =>
+        convertToDTO(certificate),
+      );
+
+      return response;
+    }, {} as IGetCertificatesDTO);
   }
 
   getCertificateTypes({ requestContext }: IGetCertificateTypes) {
@@ -21,5 +54,28 @@ export class CertificatesServices {
       useCase: this._getCertificateTypes,
       requestContext,
     });
+  }
+
+  async addCertificate({
+    input,
+    requestContext,
+  }: IAddCertificateService): Promise<CertificateDTO> {
+    try {
+      const _input = {
+        ...input,
+        startDate: normalizeDate(input.startDate),
+        endDate: normalizeDate(input.endDate),
+      };
+
+      const certificate = await executeUseCase({
+        useCase: this._addCertificate,
+        input: _input,
+        requestContext,
+      });
+
+      return convertToDTO(certificate);
+    } catch {
+      throw new AppError('Error en las fechas');
+    }
   }
 }
--- packages/server/src/domains/Certificates/Application/index.ts
+++ packages/server/src/domains/Certificates/Application/index.ts
@@ -1,1 +1,3 @@
 export * from './Certificates.service';
+export * from './DTO';
+export * from './digest';

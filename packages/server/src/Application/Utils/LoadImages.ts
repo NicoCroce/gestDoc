c9cfaadf495104173527a952b2cc5149dad3e197
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-03 12:09:25 -0300

    feat(server): load images and store in disks

---
+++ packages/server/src/Application/Utils/LoadImages.ts
@@ -0,0 +1,42 @@
+import { Request } from 'express';
+
+import fs from 'fs';
+import multer from 'multer';
+import path from 'path';
+
+const storage = (userId: number) =>
+  multer.diskStorage({
+    destination: (_req, _file, cb) => {
+      const uploadDir = path.join(__dirname, `../../../uploads/${userId}`);
+      // Crear el directorio si no existe
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+      cb(null, uploadDir);
+    },
+    filename: (_req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      const ext = path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    },
+  });
+
+// Filtro para aceptar solo imágenes
+const fileFilter = (
+  _req: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('El archivo debe ser una imagen'));
+  }
+};
+
+export const uploadImages = (userId: number) =>
+  multer({
+    storage: storage(userId),
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  });

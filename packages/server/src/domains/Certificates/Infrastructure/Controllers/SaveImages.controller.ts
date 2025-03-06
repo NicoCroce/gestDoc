dfe1c53b48d9514855d92b8affe05281ed7cc365
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-06 00:40:16 -0300

    feat(server): added controller for appending images or certificates

--- packages/server/src/domains/Certificates/Infrastructure/Controllers/SaveImages.controller.ts
+++ packages/server/src/domains/Certificates/Infrastructure/Controllers/SaveImages.controller.ts
@@ -1,5 +1,7 @@
-import { Express } from 'express';
+import { Express, Request, Response } from 'express';
 import { CertificatesServices } from '../../Application';
+import { authMiddleware } from '@server/Infrastructure/Auth/Auth';
+import { IAppendImages } from '../../Domain';
 
 export class SaveImagesController {
   constructor(
@@ -9,11 +11,28 @@ export class SaveImagesController {
     this.appendImages();
   }
 
-  appendImages = () => {
-    return this.app.post(
+  appendImages = () =>
+    this.app.post(
       '/express/load',
+      authMiddleware,
       this.certificatesService.savefilesMiddleware,
-      this.certificatesService.saveFiles,
+      async (req: Request, res: Response) => {
+        const input: IAppendImages['input'] = {
+          file: req.file,
+          host: req.get('host')!,
+          protocol: req.protocol,
+        };
+
+        try {
+          const response = await this.certificatesService.saveFiles({
+            input,
+            requestContext: req.requestContext,
+          });
+
+          res.send(response);
+        } catch (error) {
+          res.status(500).send(error);
+        }
+      },
     );
-  };
 }

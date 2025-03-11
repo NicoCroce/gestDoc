85e141a7c83f8ef33be049b9adfe9176e654a52c
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-11 18:30:40 -0300

    feat(server): added files to response DTO

--- packages/server/src/domains/Certificates/Application/digest.ts
+++ packages/server/src/domains/Certificates/Application/digest.ts
@@ -2,7 +2,7 @@ import { getDateString } from '@server/Application';
 import { Certificate } from '../Domain';
 
 export const convertToDTO = (certificate: Certificate) => {
-  const { id, startDate, endDate, type, reason } = certificate.values;
+  const { id, startDate, endDate, type, reason, files } = certificate.values;
 
   return {
     id: id!,
@@ -10,5 +10,6 @@ export const convertToDTO = (certificate: Certificate) => {
     endDate: getDateString(endDate),
     reason,
     type: type.values.name!,
+    files,
   };
 };

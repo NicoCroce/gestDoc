6eb9d22ada3029d0e2dcfe844191935e1ca6a789
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-12 00:45:38 -0300

    feat(app): added permissions

--- packages/app/src/Domains/Certificates/MenuCertificates.tsx
+++ packages/app/src/Domains/Certificates/MenuCertificates.tsx
@@ -1,9 +1,10 @@
-import { MenuItem } from '@app/Aplication';
+import { LICENSES_ACCESS, MenuItem } from '@app/Aplication';
 import { CERTIFICATES_ROUTES } from './Certificates.routes';
 import { faFileContract } from '@fortawesome/free-solid-svg-icons';
 
 export const MenuCertificates = () => (
   <MenuItem
+    permission={LICENSES_ACCESS}
     to={CERTIFICATES_ROUTES}
     icon={faFileContract}
     text="Mis Licencias"

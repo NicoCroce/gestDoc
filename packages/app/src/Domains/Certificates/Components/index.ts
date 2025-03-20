d47979e903d4632075108d68d80729ec1b4a7636
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-20 09:05:52 -0300

    feat(app):  improve certificates visualization

--- packages/app/src/Domains/Certificates/Components/index.ts
+++ packages/app/src/Domains/Certificates/Components/index.ts
@@ -1,3 +1,4 @@
 export * from './AddLicenseForm';
+export * from './Certificate';
 export * from './CertificatesGrid';
 export * from './NewLicenseButton';
--- packages/app/src/Domains/Certificates/Pages/CertificateList.page.tsx
+++ packages/app/src/Domains/Certificates/Pages/CertificateList.page.tsx
@@ -1,4 +1,4 @@
-import { Container, Page, Text } from '@app/Aplication';
+import { Container, Page, Title } from '@app/Aplication';
 import { CertificatesGrid } from '../Components/CertificatesGrid';
 
 import { v4 as uuidv4 } from 'uuid';
@@ -15,8 +15,12 @@ export const CertificateListPage = () => {
       {data &&
         Object.entries(data).map(([year, certificates]) => (
           <Container key={uuidv4()} space="large">
-            <Text.Lead>Certificados correspondientes al año {year}</Text.Lead>
-            <CertificatesGrid certificatesList={certificates} />
+            <Title variant="h4">
+              Certificados correspondientes al año {year}
+            </Title>
+            <Container block className="md:mx-14">
+              <CertificatesGrid certificatesList={certificates} />
+            </Container>
           </Container>
         ))}
     </Page>

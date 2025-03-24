f1100127089cd42ce7774410734dcce60a613495
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-24 13:39:22 -0300

    feat(app): added filters to service then get certificates

--- packages/app/src/Domains/Certificates/Certificate.entity.ts
+++ packages/app/src/Domains/Certificates/Certificate.entity.ts
@@ -3,3 +3,9 @@ import { CertificateDTO } from '@server/domains/Certificates';
 export interface ICertificate extends CertificateDTO {
   type: string;
 }
+
+export type TCertificatesSearch = {
+  employee?: string;
+  date?: string;
+  type?: string;
+};
--- packages/app/src/Domains/Certificates/Hooks/useGetCertificates.ts
+++ packages/app/src/Domains/Certificates/Hooks/useGetCertificates.ts
@@ -1,8 +1,16 @@
+import { useURLParams } from '@app/Aplication';
 import { CertificatesService } from '../Certificates.service';
+import { TCertificatesSearch } from '../Certificate.entity';
 
 export const useGetCertificates = () => {
-  return CertificatesService.getAll.useQuery(undefined, {
-    staleTime: 1 * 60,
+  const { searchParams } = useURLParams<TCertificatesSearch>();
+
+  const input =
+    (searchParams && { ...searchParams, type: Number(searchParams?.type) }) ||
+    undefined;
+
+  return CertificatesService.getAll.useQuery(input, {
+    staleTime: 3000,
     refetchOnWindowFocus: true,
   });
 };

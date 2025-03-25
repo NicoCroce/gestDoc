28e40825c66ba34dea2b5846f9dff99d3a36e152
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-24 22:20:20 -0300

    feat(app): added filters in hook to get Certificates by Company

--- packages/app/src/Domains/Admin/Hooks/useGetCertificatesByCompany.ts
+++ packages/app/src/Domains/Admin/Hooks/useGetCertificatesByCompany.ts
@@ -1,9 +1,21 @@
-import { CertificatesService } from '@app/Domains/Certificates';
+import { useURLParams } from '@app/Aplication';
+import {
+  CertificatesService,
+  TCertificatesSearch,
+} from '@app/Domains/Certificates';
 
-export const useGetCertificatesByCompany = () =>
-  CertificatesService.getCertificatesByCompany.useQuery(undefined, {
+export const useGetCertificatesByCompany = () => {
+  const { searchParams } = useURLParams<TCertificatesSearch>();
+
+  const input =
+    (searchParams && { ...searchParams, type: Number(searchParams?.type) }) ||
+    undefined;
+
+  return CertificatesService.getCertificatesByCompany.useQuery(input, {
     staleTime: 3000, // Los datos no se refetchan durante 3 segundos
+    refetchOnWindowFocus: true,
   });
+};
 
 export type TuseGetCertificatesByCompany = ReturnType<
   typeof useGetCertificatesByCompany

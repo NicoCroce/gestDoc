aa38fb16d03c121d64fa5e2c781d1119859ab7d8
Nico Croce <ingcrocenicolas@outlook.com>
2025-03-24 22:20:53 -0300

    feat(app): added filters in Certificates by Company Page

--- packages/app/src/Domains/Admin/Pages/LicensesCompany.page.tsx
+++ packages/app/src/Domains/Admin/Pages/LicensesCompany.page.tsx
@@ -1,15 +1,25 @@
-import { Container, Page } from '@app/Aplication';
+import { Container, FilterButton, FiltersSheet, Page } from '@app/Aplication';
 import { Statistics } from '../../Documents/Components';
 
 import { useGetCertificatesByCompany } from '../Hooks';
 import { LicensesListWrapper } from '../Components';
+import { useState } from 'react';
+import { FiltersCertificatesForm } from '@app/Domains/Certificates';
 
 export const LicensesCompanyPage = () => {
   //const { isMobile } = useDevice();
   const service = useGetCertificatesByCompany();
+  const [filtersIsOpen, setFiltersIsOpen] = useState(false);
+
+  const handleFilters = () => {
+    setFiltersIsOpen((prevState) => !prevState);
+  };
 
   return (
-    <Page title="Todos los documentos de la empresa">
+    <Page
+      title="Todos los documentos de la empresa"
+      headerRight={<FilterButton onClick={handleFilters} variant="secondary" />}
+    >
       <Container>
         <Statistics />
         <Container row>
@@ -17,6 +27,13 @@ export const LicensesCompanyPage = () => {
             <LicensesListWrapper service={service} />
           </div>
         </Container>
+        <FiltersSheet
+          open={filtersIsOpen}
+          closeSheet={handleFilters}
+          title="Filtros de Certificados"
+        >
+          <FiltersCertificatesForm />
+        </FiltersSheet>
       </Container>
     </Page>
   );

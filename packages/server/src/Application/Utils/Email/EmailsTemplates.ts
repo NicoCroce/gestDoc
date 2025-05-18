a1bcaebb57fed7cf947d0f8d41a942a052a29bb1
Nico Croce <ingcrocenicolas@outlook.com>
2025-05-18 10:15:20 -0300

    feat(server): added template for addLicence and sign Document

---
+++ packages/server/src/Application/Utils/Email/EmailsTemplates.ts
@@ -0,0 +1,37 @@
+interface IAddLincence {
+  currentUser: string;
+  reason: string;
+}
+
+interface ISignDocument {
+  currentUser: string;
+  reason: string;
+  documentId: number;
+}
+
+const addLicence = ({ currentUser, reason }: IAddLincence) => ({
+  subject: `[Aviso] Gestdoc - Nueva licencia de ${currentUser}`,
+  body: `<h1> Nueva licencia</h1> 
+              <p>El empleado <strong> ${currentUser} </strong> agregó una licencia<p>
+              <h2>Descripción de la licencia</h2>
+              <p>${reason}</p>
+              <hr>
+              <p>Este mail fue enviado de forma automática por <strong><a href="https://docs.macrosistemas.ar/" target="_blank" rel="nofollow">GestDoc</a></strong></p>
+              `,
+});
+
+const signDocument = ({ currentUser, reason, documentId }: ISignDocument) => ({
+  subject: `[Aviso] Gestdoc - Firma bajo no conformidad de ${currentUser}`,
+  body: `<h1> Nueva firma bajo no conformidad</h1> 
+              <p>El empleado <strong>${currentUser}</strong> firmó el documento ${documentId} <p>
+              <h2>Motivo de la firma</h2>
+              <p>${reason}</p>
+              <hr>
+              <p>Este mail fue enviado de forma automática por <strong> <a href="https://docs.macrosistemas.ar/" target="_blank" rel="nofollow">GestDoc</a></strong></p>
+              `,
+});
+
+export const emailTemplates = {
+  addLicence,
+  signDocument,
+};

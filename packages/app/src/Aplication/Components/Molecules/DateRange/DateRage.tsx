6d661ed08a47e916715d64d8b396a137cef12115
Nico Croce <ingcrocenicolas@outlook.com>
2025-06-06 00:18:05 -0300

    feat(app): improve date range for mobile

--- packages/app/src/Aplication/Components/Molecules/DateRange/DateRage.tsx
+++ packages/app/src/Aplication/Components/Molecules/DateRange/DateRage.tsx
@@ -1,10 +1,4 @@
-import {
-  Control,
-  FieldValues,
-  Path,
-  PathValue,
-  UseFormReturn,
-} from 'react-hook-form';
+import { FieldValues, Path, UseFormReturn } from 'react-hook-form';
 import {
   FormControl,
   FormField,
@@ -12,86 +6,64 @@ import {
   FormLabel,
   FormMessage,
 } from '../../ui/form';
-import { DatePickerWithRange } from './DatePickerWithRange';
-import { DateRange as DT } from 'react-day-picker';
-import { useDevice } from '@app/Aplication/Hooks';
 import { Input } from '../Input';
 import { Container } from '../../Layout';
 
 interface DateRangeProps<T extends FieldValues> {
   nameStartDate: Path<T>;
   nameEndDate: Path<T>;
-  control: Control<T>;
   label: string;
   form: UseFormReturn<T>;
 }
 
 export const DateRange = <T extends FieldValues>({
-  control,
   nameStartDate,
   nameEndDate,
   form,
 }: DateRangeProps<T>) => {
-  const { isMobile } = useDevice();
-
   const startDate = form.watch(nameStartDate);
 
-  const handleDate = (date: DT) => {
-    form.setValue(nameStartDate, date.from! as PathValue<T, Path<T>>);
-    form.setValue(nameEndDate, date.to! as PathValue<T, Path<T>>);
-  };
-
   return (
-    <>
-      {isMobile ? (
-        <Container>
-          <Container row justify="between">
-            <FormField
-              name={nameStartDate}
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Fecha de inicio</FormLabel>
-                  <FormControl>
-                    <Input type="date" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name={nameEndDate}
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Fecha de fin</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="date"
-                      {...field}
-                      disabled={startDate === undefined}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </Container>
-        </Container>
-      ) : (
+    <Container>
+      <Container row justify="between" className="[&>*]:flex-1">
         <FormField
-          name={nameEndDate}
-          control={control}
-          render={() => (
+          name={nameStartDate}
+          control={form.control}
+          render={({ field }) => (
             <FormItem>
-              <FormLabel>Seleccione rango de fecha de licencia</FormLabel>
+              <FormLabel>Fecha de inicio</FormLabel>
               <FormControl>
-                <DatePickerWithRange onChangeDate={handleDate} />
+                <Input
+                  {...field}
+                  type="date"
+                  className="block"
+                  value={field.value || ''}
+                />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
-      )}
-    </>
+        <FormField
+          name={nameEndDate}
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Fecha de fin</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="date"
+                  value={field.value || ''}
+                  className="block"
+                  disabled={startDate === undefined}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </Container>
+    </Container>
   );
 };
